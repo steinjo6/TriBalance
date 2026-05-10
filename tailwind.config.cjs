0412d5156aab08ef88d971da20e205b5823ce920
Joel Steinemann <steinjo6@students.zhaw.ch>
2026-05-10 16:57:43 +0200

    fix: database connection established, starting layout and error debugging

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+const { join } = require('path');
+const { skeleton } = require('@skeletonlabs/tw-plugin');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+				],
+			},
+		}),
+	],
+};
